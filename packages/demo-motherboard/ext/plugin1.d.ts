719b489afd852377013b16f85a09315b337d7207
Tao Wen <taowen@chengfayun.com>
2022-02-26 11:47:38 +0800

    explain why type looks complex

--- packages/demo-motherboard/ext/plugin1.d.ts
+++ packages/demo-motherboard/ext/plugin1.d.ts
@@ -2,6 +2,8 @@
 // demo-plugin1 can not decide what is interface alone
 // demo-motherboard tightly controls what is visible for other plugin
 declare module "@plugin1" {
+    // this is type declaration of vue component
+    // if you are using react, the declaration is simpler
     export const ComponentProvidedByPlugin1: {
         new(): {
             $props: {
@@ -13,5 +15,6 @@ declare module "@plugin1" {
             onClick():void;
         }
     }
+    // plugin can export anything, of couse it can export a simple function
     export function spiExportedByPlugin1ForOtherPlugins(): string;
 }
--- packages/demo-motherboard/ext/plugin2.d.ts
+++ packages/demo-motherboard/ext/plugin2.d.ts
@@ -1,4 +1,6 @@
 declare module "@plugin2" {
+    // this is type declaration of vue component
+    // if you are using react, the declaration is simpler
     export const ComponentProvidedByPlugin2: {
         new(): {
             $props: {
